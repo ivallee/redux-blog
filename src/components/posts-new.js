d3218a0748288bda51d1a2bc1cb966236b95e460
ivallee <isaacvallee@gmail.com>
2018-01-25 19:10:58 -0800

    add form fields

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -2,18 +2,47 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className='form-group'>
+        <label>{field.label}</label>
+        <input
+          className='form-control' 
+          type='text'
+          {...field.input}
+        />
+      </div>
+    );
+  }
+  
   render() {
     return (
       <form>
         <Field 
+          label='Title'
           name='title'
-          component={}
+          component={this.renderField}
         /> 
+        <Field 
+          label='Categories'
+          name='categories'
+          component={this.renderField}
+        />
+        <Field 
+          label='Post Content'
+          name='content'
+          component={this.renderField}
+        />
       </form>
     );
   }
 }
 
+function validate(values) {
+  console.log(values);
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
