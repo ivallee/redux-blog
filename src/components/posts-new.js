d312dadecd69b12afe4ad111fa145bb9817a0d29
ivallee <isaacvallee@gmail.com>
2018-01-25 20:09:53 -0800

    form validation and styling

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { label, meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+    
     return (
-      <div className='form-group'>
-        <label>{field.label}</label>
+      <div className={className}>
+        <label>{label}</label>
         <input
           className='form-control' 
           type='text'
           {...field.input}
         />
+        <div className='text-help'>
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
   
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+ 
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field 
           label='Title'
           name='title'
@@ -33,13 +46,29 @@ class PostsNew extends Component {
           name='content'
           component={this.renderField}
         />
+        <button type='submit' className='btn btn-primary'>Submit</button>
+        <Link to='/' className='btn btn-danger'>Cancel</Link>
       </form>
     );
   }
 }
 
 function validate(values) {
-  console.log(values);
+  const errors = {};
+
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter at least one category";
+  }
+
+  if (!values.content) {
+    errors.content = "You need to add some content";
+  }
+  
+  return errors;
 }
 
 export default reduxForm({
